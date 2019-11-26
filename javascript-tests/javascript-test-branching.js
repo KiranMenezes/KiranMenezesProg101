e3e85f534f34d65c474f53b5737a352c57a41c93
Kiran Menezes <kiran.menezes@bellevuecollege.edu>
2019-11-25 22:40:03 -0800

    branching

--- javascript-tests/javascript-test-branching.js
+++ javascript-tests/javascript-test-branching.js
@@ -1,3 +1,4 @@
+/*
 const height = 5;
 if (height >= 5) {
     console.log('We have a big one');
@@ -21,7 +22,7 @@ if (height >= 5) {
 
 height = 'little';
 
-switch(height) {
+switch (height) {
     case 'little':
         console.log('Switch: a little one is smaller than twentyfive');
         break;
@@ -33,17 +34,56 @@ switch(height) {
     default:
         console.log('We do not know about this one.')
 }
+*/
 
 window.onload = () => {
     const simpleBranchAction = document.getElementById('simpleBranchAction');
     const simpleBranchDisplay = document.getElementById('simpleBranchDisplay');
-    
+
     simpleBranchAction.onclick = (event) => {
+
+        console.log('SIMPLE branch ACTION ON CLICK WAS CALLED');
         let height = 4;
-        if (height >= 5) {
+        if (height >= 25) {
             simpleBranchDisplay.textContent = 'We have a big one';
         } else {
             simpleBranchDisplay.textContent = 'We have a little one';
-        }   
+        }
     }
-}    
+    simpleifelseAction.onclick = (event) => {
+        console.log('SIMPLE if-else ACTION ON CLICK WAS CALLED');
+        const height = 50;
+        if (height >= 25) {
+            simpleifelseDisplay.textContent = 'We have a big one';
+            console.log('We have a big one');
+        } else if (height >= 0) {
+            simpleifelseDisplay.textContent = 'We have a little one';
+            console.log('We have a little one');
+        } else {
+            simpleifelseDisplay.textContent = 'We have an error';
+            console.log('We have a negative number or invalid number')
+        }
+
+    }
+    simpleswitchAction.onclick = (event) => {
+        console.log('SIMPLE switch ACTION ON CLICK WAS CALLED');
+        height = 'little';
+
+        switch (height) {
+            case 'little':
+                simpleswitchDisplay.textContent = 'Switch: a little one is smaller than twentyfive';
+                console.log('Switch: a little one is smaller than twentyfive');
+                break;
+
+            case ('big'):
+                simpleswitchDisplay.textContent = 'Switch: a big one is twentyfive or greater';
+                console.log('Switch: a big one is twentyfive or greater');
+                break;
+
+            default:
+                console.log('We do not know about this one.')
+        }
+    }
+}
+
+
