676b594d51b902872e27858061bf86d26b15fb30
Kiran Menezes <kiran.menezes@bellevuecollege.edu>
2019-12-01 20:15:00 -0800

    radio-choices

--- javascript-tests/radio-choices.js
+++ javascript-tests/radio-choices.js
@@ -1,32 +1,44 @@
-const myForm = document.getElementById('sort-type');
+window.onload = () => {
+    const myForm = document.getElementById('sort-type');
 
-myForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const value = document.querySelector('input[name="sorter"]:checked').value;
-    if (value.toLowerCase() === 'green') {
-        setColor("blueBackground");
-        showChoice(value);
-    } else {
-        setColor("greenBackground");
-        showChoice(value);
+    myForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const value = document.querySelector('input[name="sorter"]:checked').value;
+        if (value.toLowerCase() === 'green') {
+            setColor("greenBackground");
+            showChoice(value);
+        } else if (value.toLowerCase() === 'blue') {
+            setColor("blueBackground");
+            showChoice(value);
+        } else if (value.toLowerCase() === 'red') {
+            setColor("redBackground");
+            showChoice(value);
+
+        } else if (value.toLowerCase() === 'yellow') {
+            setColor("yellowBackground");
+            showChoice(value);
+
+        } else if (value.toLowerCase() === 'purple') {
+            setColor("purpleBackground");
+            showChoice(value);
+        }
+    });
+
+    let currentClass = null;
+
+    const setColor = (className) => {
+        console.log("SET CLASS TO", className);
+        var element = document.getElementById("form-selection");
+        if (currentClass) {
+            element.classList.remove(currentClass);
+        }
+        currentClass = className;
+        element.classList.add(className);
     }
-});
 
-let currentClass = null;
-
-const setColor = (className) => {
-    console.log("SET CLASS TO", className);
-    var element = document.getElementById("form-section");
-    if (currentClass) {
-        element.classList.remove(currentClass);
+    const showChoice = (choice) => {
+        console.log("CHOICE", choice);
+        var element = document.getElementById('user-choice');
+        element.textContent = choice;
     }
-    currentClass = className;
-    element.classList.add(className);
-}
-if (value.toLowerCase() === 'green') {
-    setColor("blueBackground");
-    showChoice(value);
-} else {
-    setColor("greenBackground");
-    showChoice(value);
-}
+};
