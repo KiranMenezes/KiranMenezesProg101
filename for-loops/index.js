66ddd92c89653522252be6315b331fd08cf19158
Kiran Menezes <kiran.menezes@bellevuecollege.edu>
2019-12-01 17:45:03 -0800

    for-loops

---
+++ for-loops/index.js
@@ -0,0 +1,18 @@
+window.onload = () => {
+    const listButton = document.getElementById('runForLoop');
+
+    listButton.onclick = () => {
+        const myList = document.getElementById('myListt');
+            // WRAP THE NEXT LINE IN A for-loop that iterates over a variable named i.
+            // The loop should iterate 5 times (count from zero to five).
+            elfCode.appendToList(myList, i);
+            // END LOOP
+    }
+    const elfCode = {
+        appendToList: (list, value) => {
+            const li = document.createElement("li");
+            li.appendChild(document.createTextNode(value));
+            list.appendChild(li);
+        }
+    };
+}
